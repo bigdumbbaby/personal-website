7c603ae8433d703619d2b903979e83e31f713f95
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-04-13 20:23:43 -0600

    first commit

---
+++ src/components/pages/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Footer.css';
+import { Button } from '../../Button';
+import { Link } from 'react-router-dom';
+import {
+  FaGithub,
+  FaLinkedin
+} from 'react-icons/fa';
+import { MdFingerprint } from 'react-icons/md';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='social-icons'>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+              onClick={() => window.location.assign('https://www.linkedin.com/in/colton-o-connor/')}
+            >
+              <FaLinkedin size='2x' className='footer-icon'/>
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='GitHub'
+              onClick={() => window.location.assign('https://github.com/bigdumbbaby/')}
+            >
+              <FaGithub size='2x' className='footer-icon'/>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
