beff274a112cb8f478f79fa89962c893556636c8
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-04-18 14:22:40 -0600

    fixed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,33 +7,52 @@ import {
   FaLinkedin
 } from 'react-icons/fa';
 import { MdFingerprint } from 'react-icons/md';
+import { HiOutlineMail } from 'react-icons/hi';
 
 function Footer() {
   return (
     <div className='footer-container'>
       <section className='social-media'>
+        <h2 className='footer-title' 
+        // style={{
+        //   color: 'white',
+        // }}
+        >Connect with Me</h2>
         <div className='social-media-wrap'>
-          <div className='social-icons'>
-            <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-              onClick={() => window.location.assign('https://www.linkedin.com/in/colton-o-connor/')}
-            >
-              <FaLinkedin size='2x' className='footer-icon'/>
-            </Link>
-            <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='GitHub'
-              onClick={() => window.location.assign('https://github.com/bigdumbbaby/')}
-            >
-              <FaGithub size='2x' className='footer-icon'/>
-            </Link>
+            <div className='icon-container'>
+              <Link
+                className='social-icon-link'
+                to='/'
+                target='_blank'
+                aria-label='LinkedIn'
+                onClick={() => window.location.assign('https://www.linkedin.com/in/colton-o-connor/')}
+              >
+                <FaLinkedin className='footer-icon' size='1x'/>
+              </Link>
+            </div>
+            <div className='icon-container'>
+              <Link
+                className='social-icon-link'
+                to='/'
+                target='_blank'
+                aria-label='LinkedIn'
+                onClick={() => window.location.assign('https://mail.google.com/mail/u/0/')}
+              >
+                <HiOutlineMail className='footer-icon' size='1x'/>
+              </Link>
+            </div>
+            <div className='icon-container'>
+              <Link
+                className='social-icon-link'
+                to='/'
+                target='_blank'
+                aria-label='GitHub'
+                onClick={() => window.location.assign('https://github.com/bigdumbbaby/')}
+              >
+                <FaGithub className='footer-icon' size='1x'/>
+              </Link>
+            </div>
           </div>
-        </div>
       </section>
     </div>
   );
